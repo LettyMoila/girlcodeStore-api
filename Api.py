f4abe8e3901736da20b4dfbae95580770841e905
LettyMoila <moilankibelo@gmail.com>
2023-07-05 03:22:08 +0200

    added image route

--- Api.py
+++ Api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask_cors import CORS
 
 import config
@@ -13,25 +13,32 @@ def main():
 
 @app.route('/categories')
 def categories_route():
-    top_categories = []
+        top_categories = []
 
-    cat_instance = db('categories')
+        cat_instance = db('categories')
 
-    rows = cat_instance.select()
+        rows = cat_instance.select()
 
-    for row in rows:
-        temp_cat = {
-            "id": row[0],
-            "name": row[1],
-            "slug": row[2],
-            "desc": row[3],
-            "img": row[4]
-        }
+        for row in rows:
+            temp_cat = {
+                "id": row[0],
+                "name": row[1],
+                "slug": row[2],
+                "desc": row[3],
+                "img": row[4]
+                }
     
-    top_categories.append(temp_cat)
+        top_categories.append(temp_cat)
 
-    categories_dict = {
-        "categories": top_categories
-    }
+        categories_dict = {
+            "categories": top_categories
+        }
 
-    return  categories_dict
+        return  categories_dict
+
+@app.route('/images/<img_name>')
+def get_image(img_name):
+    print(img_name)
+    filename = f'images/(img_name)'
+
+    return send_file(filename, minetype='image/jpg')
