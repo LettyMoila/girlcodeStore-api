2cf6a0d07f6c59d906bd3b42ce48d05c80d53a55
LettyMoila <moilankibelo@gmail.com>
2023-07-14 09:48:23 +0200

    updated api file

--- Api.py
+++ Api.py
@@ -1,7 +1,8 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from flask_cors import CORS
 
 import config
+
 from db import db
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ def main():
 
 @app.route('/categories')
 def categories_route():
-        top_categories = []
+        temp_categories = []
 
         cat_instance = db('categories')
 
@@ -28,10 +29,10 @@ def categories_route():
                 "img": row[4]
                 }
     
-        top_categories.append(temp_cat)
+        temp_categories.append(temp_cat)
 
         categories_dict = {
-            "categories": top_categories
+            "categories": temp_categories
         }
 
         return  categories_dict
@@ -58,6 +59,47 @@ def category_route(cat_slug):
     else:
         return {}
 
+@app.route('/category/<cat_slug>/products')
+def category_products_route(cat_slug):
+    cat_prods_inst = db('categories_products')
+
+    columns="products.*"
+
+    joins_list = [
+        {
+            "table": "categories",
+            "on_cond": "categories_products.cat_id=categories.id"
+        },
+        {
+            "table": "products",
+            "on_cond": "categories_products.prod_id=products.id"
+        }
+    ]
+
+    condition = f"WHERE categories.slug='{cat_slug}'"
+
+    rows = cat_prods_inst.select(columns,condition, joins_list)
+    temp_products = []
+
+    for row in rows:
+        temp_prod = {
+            "id": row[0],
+            "prod_name": row[1],
+            "prod_img": row[2],
+            "prod_desc": row[3],
+            "prod_slug": row[4],
+            "prod_price": row[5]
+        }
+
+        temp_products.append(temp_prod)
+
+    temp_cat_prods = {
+        "cat_slug": cat_slug,
+        "products": temp_products
+    }
+
+    return temp_cat_prods
+
 @app.route('/products')
 def products_route():
      prod_instance = db('products')
@@ -83,58 +125,42 @@ def products_route():
           "products": temp_products
      }
 
-     return temp_products
+     return products_dict
 
 @app.route('/product/<prod_slug>')
-def categories_route():
-        top_categories = []
-
-        cat_instance = db('categories')
-
-        rows = cat_instance.select()
-
-        for row in rows:
-            temp_cat = {
-                "id": row[0],
-                "name": row[1],
-                "slug": row[2],
-                "desc": row[3],
-                "img": row[4]
-                }
-    
-        top_categories.append(temp_cat)
-
-        categories_dict = {
-            "categories": top_categories
-        }
-
-        return  categories_dict
-
-@app.route('/category/<cat_slug>')
 def product_route(prod_slug):
+        prod_instance = db('prod_slug')
 
-    prod_instance = db('categories')
+        rows = prod_instance.select(condition=f"WHERE prod_slug='{prod_slug}'")
 
-    rows = prod_instance.select(f"WHERE prod-slug='{prod_slug}'")
+        if len(rows):
+            row = rows[0]
 
-    if len(rows):
-        row = rows[0]
-        
-        temp_prod = {
-             "id": row[0],
-             "prod_name": row[1],
-             "prod_img": row[2],
-             "prod_desc": row[3],
-             "prod_slug": row[4]
-        }
+            temp_prod = {
+                "id": row[0],
+                "prod_name": row[1],
+                "prod_img": row[2],
+                "prod_desc": row[3],
+                "prod_slug": row[4],
+                "prod_price": row[5]
+            }
 
-        return temp_prod
-    else:
-        return {}
+            return temp_prod
+        else:
+            return {}
 
-@app.route('/images/<img_name>')
+@app.route('/image/<img_name>')
 def get_image(img_name):
-    print(img_name)
-    filename = f'images/(img_name)'
+    filename = f'images/{img_name}'
 
-    return send_file(filename, minetype='image/jpg')
+    return send_file(filename, mimetype='image/jpg')
+
+@app.route('/submit_checkout', methods=["POST"])
+def submit_checkout_route():
+    data = request.form
+
+    fullname = data['fullname']
+    emailaddress = data['emailaddress']
+    cart = data['cart']
+
+    return f'Submission from {fullname} {emailaddress}, their cart {cart}'
