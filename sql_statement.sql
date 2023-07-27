d99df30b24e4cd79610b87ed021ffcbadeb790b9
LettyMoila <moilankibelo@gmail.com>
2023-07-27 15:01:55 +0200

    added products

--- sql_statement.sql
+++ sql_statement.sql
@@ -15,14 +15,14 @@ VALUES ('Furniture', 'furniture'),
 	('Sneaker', 'sneaker'),
 	('Travel', 'travel');
 	
-	--Add cat_img column on categories
-	ALTER TABLE categories
-	ADD cat_img TEXT;
+--Add cat_img column on categories
+ALTER TABLE categories
+ADD cat_img TEXT;
 	
-	UPDATE categories
-	SET cat_img='cat_img.jpg';
+UPDATE categories
+SET cat_img='cat_img.jpg';
 	
-	--create products table
+--create products table
 	CREATE TABLE products (
 	id SERIAL PRIMARY KEY,
 	prod_name TEXT NOT NULL UNIQUE,
@@ -34,7 +34,7 @@ VALUES ('Furniture', 'furniture'),
 
 --Insert mini ipod product
 INSERT INTO products (prod_name, prod_img, prod_desc, prod_slug, prod_price)
-VALUES ('Insta Mini 9', 'insta-mini-9.png', 'Selfie mode and selfie mirrow, Macro mode', 'insta-mini-9', 99.85);
+VALUES ('Insta Mini 9', 'instax-mini-9.jpg', 'Selfie mode and selfie mirrow, Macro mode', 'insta-mini-9', 99.85);
 
 --create table for product from category
 CREATE TABLE categories_products (
@@ -66,3 +66,14 @@ CREATE TABLE cart (
 	client_id INT NOT NULL,
 	FOREIGN KEY(client_id) REFERENCES client(id)
 );
+
+--updating my instax image
+UPDATE products
+SET prod_img = 'instax-mini-9.jpg'
+WHERE id = 1;
+
+-- updating all categories
+UPDATE categories
+SET cat_img = 'travel.jpg'
+WHERE id = 6;
+
